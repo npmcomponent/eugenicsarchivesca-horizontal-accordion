9f95c8ebeec859b3871acf9cb5225b81df0b05f1
Ben McMahen <ben.mcmahen@gmail.com>
2013-04-06 13:22:32 -0600

    first draft

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# pamphlet
+
+  a horizontal accordian, used like a pamphlet'
+
+## Installation
+
+    $ component install bmcmahen/pamphlet
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,83 @@
+var Emitter = require('component-emitter')
+	, bind = require('component-bind')
+	, events = require('component-events')
+	, classes = require('component-classes');
+
+var Pamphlet = function(selector){
+	if (!(this instanceof Pamphlet)) return new Pamphlet(selector);
+	if (!el) throw new TypeError('Pamphlet() requires an element');
+	this.el = document.querySelector(selector);
+	var panes = this.el.querySelectorAll('.pamphlet-panes');
+
+	// Maintain a collection of child elements
+	this.children = [];
+	for (var i = 0; i < panes.length; i++){
+		this.children.push(new PamphletPane(this, panes[i]));
+	}
+	this.total = this.children.length;
+	this.width = this.determineSize();
+	this.on('pane-clicked', bind(this, this.onPaneClicked));
+};
+
+Emitter(Pamphlet.prototype);
+
+Pamphlet.prototype.determineSize = function(){
+	var rect = this.el.getBoundingClientRect();
+	return rect.right - rect.left;
+};
+
+Pamphlet.prototype.determineChildSize = function(){
+	if (!this.selected){}
+};
+
+Pamphlet.prototype.onPaneClicked = function(pane){
+	if (this.selected) this.selected.removeActive();
+	this.selected = pane;
+	if (pane.collapsed) pane.removeCollapse();
+	for (var i = 0; i < this.children.length; i++) {
+		var child = this.children[i];
+		if (! child.selected) {
+			child.collapse();
+		}
+	}
+};
+
+// Each Pamphlet Pane
+var PamphletPane = function(context, el){
+	if (!(this instanceof PamphletPane)) return new PamphletPane(context, el);
+	if (!el) throw new TypeError('PamphletPane() requires an element');
+	this.el = el;
+	this.context = context;
+};
+
+Emitter(PamphletPane.prototype);
+
+PamphletPane.prototype.bind = function(){
+	this.events = events(this.el, this);
+	this.events.bind('click');
+};
+
+PamphletPane.prototype.onclick = function(e){
+	this.context.emit('pane-clicked', this);
+	this.makeActive();
+};
+
+PamphletPane.prototype.removeActive = function(){
+	this.selected = false;
+	classes(this.el).remove('active');
+};
+
+PamphletPane.prototype.makeActive = function(){
+	this.selected = true;
+	classes(this.el).add('active');
+};
+
+PamphletPane.prototype.collapse = function(){
+	this.collapseed = true;
+	classes(this.el).add('collapsed');
+};
+
+PamphletPane.prototype.removeCollapse = function(){
+	this.collapsed = false;
+	classes(this.el).remove('collapsed');
+};
