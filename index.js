9e4472813a23e41fe1d753d9c1c791642881e450
Ben McMahen <ben.mcmahen@gmail.com>
2013-04-08 13:08:20 -0600

    reverting back to using classes. no real performance advantange gained using abs positioning, surprisingly.

--- index.js
+++ index.js
@@ -16,41 +16,9 @@ var Accordion = module.exports = function(selector){
     this.children.push(new AccordionPane(this, panes[i]));
   }
   this.on('click', bind(this, this.onPaneClicked));
-  this.determineSize().determinePosition();
 };
 
 Emitter(Accordion.prototype);
-
-// Calculate the size of our box, and the size of each
-// potential pane, including regular, active, collapsed.
-Accordion.prototype.determineSize = function(){
-  var rect = this.el.getBoundingClientRect();
-  this.width = rect.right - rect.left;
-  this.paneWidth = this.width / this.children.length;
-  this.activeWidth = 0.75 * this.width;
-  this.collapsedWidth = (this.width - this.activeWidth) / (this.children.length - 1);
-  return this;
-};
-
-// Determine our Left & Width of each pane.
-Accordion.prototype.determinePosition = function(){
-  var left = 0;
-  for (var i = 0; i < this.children.length; i++) {
-    var child = this.children[i];
-    child.setLeft(left);
-    if (child.isActive) {
-      left += this.activeWidth;
-      child.setRight(this.activeWidth);
-    } else if (child.isCollapsed) {
-      left += this.collapsedWidth;
-      child.setRight(this.collapsedWidth);
-    } else {
-      left += this.paneWidth;
-      child.setRight(this.paneWidth);
-    }
-  }
-  return this;
-};
 
 Accordion.prototype.onPaneClicked = function(pane){
   if (this.selected) this.selected.removeActive();
@@ -73,19 +41,6 @@ var AccordionPane = function(context, el){
 };
 
 Emitter(AccordionPane.prototype);
-
-// Manage Position
-AccordionPane.prototype.setLeft = function(l){
-  var s = this.el.style;
-  this.left = l;
-  s.left = l + 'px';
-};
-
-AccordionPane.prototype.setRight = function(w){
-  var s = this.el.style;
-  this.right = w;
-  s.right = this.context.width - (this.left + w) + 'px';
-};
 
 // Bind Events
 AccordionPane.prototype.bind = function(){
